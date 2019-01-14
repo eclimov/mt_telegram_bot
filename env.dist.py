6cc6b5d8120de1caebdf2445d55661f947a87f6e
Eduard Climov <klimovedik88@gmail.com>
2019-01-14 23:48:54 +0300

    - Filled "env.dist.py" config template

--- env.dist.py
+++ env.dist.py
@@ -1,3 +1,28 @@
 # Purpose of this file is to show structure of "env.py". Do not use it in production
 
-# TODO: fill it
+telegram_bot_token = ''
+google_spreadsheet_keyfile_dict = {
+    "type": "",
+    "project_id": "",
+    "private_key_id": "",
+    "private_key": "",
+    "client_email": "",
+    "client_id": "",
+    "auth_uri": "",
+    "token_uri": "",
+    "auth_provider_x509_cert_url": "",
+    "client_x509_cert_url": ""
+}
+google_spreadsheet_url = ''
+
+about = {
+    'info': "",
+    'website': ""
+}
+help_text = ""
+
+paid_day_offs = {
+    # name: date
+}
+
+
